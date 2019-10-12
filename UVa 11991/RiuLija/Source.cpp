2ee55ebd20ab7fa29c946f77abf85562034eb416
dagil02 <dagil02@ucm.es>
2019-10-12 11:45:07 +0200

    Problemas en la torre

---
+++ UVa 11991/RiuLija/Source.cpp
@@ -0,0 +1,67 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+//#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+
+using namespace std;
+
+// función que resuelve el problema
+void resolver(int n, int m) {
+	vector< vector<int> > v = vector< vector<int> >(1000001, vector<int>(0));
+
+	for (int i = 1; i <= n; i++) {
+		int x;
+		cin >> x;
+		v[x].push_back(i);
+	}
+
+	for (int i = 0; i < m; i++) {
+		int num, pos;
+		cin >> pos >> num;
+		if (v[num].size() < pos) cout<< 0 <<'\n';
+		else cout << v[num][pos - 1] << '\n';
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int n, m;
+	cin >> n >> m;
+	if (!std::cin)
+		return false;
+
+	resolver(n,m);
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Uva 10566/crossedLadders/Source.cpp
@@ -0,0 +1,65 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+// función que resuelve el problema
+double resolver(double &a, double &b, double &h) {
+	double minimo, maximo, w, estimacion, diff = 0.000001;
+	minimo = 0;
+	maximo = min(a, b);
+
+	while (true) {
+		w = minimo / 2 + maximo / 2;
+		estimacion = sqrt(a*a - w * w)*sqrt(b*b - w * w) / (sqrt(a*a - w * w) + sqrt(b*b - w * w));
+		if (fabs(estimacion - h) <= diff)
+			return w;
+		if (estimacion > h)minimo = w;
+		else maximo = w;
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	double a, b, h;
+	cin >> a >> b >> h;
+	if (!std::cin)
+		return false;
+
+	double sol = resolver(a,b,h);
+	
+	printf("%.3f\n", sol);
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
